eda7a320cf1a7d281595c4e8a083807da01a7aaa
lksheehan <55460848+lksheehan@users.noreply.github.com>
2020-09-02 10:25:43 -0400

    added everything

---
+++ Assets/Scripts/Platforms.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+    public float speed;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("moving!");
+        transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    public float JumpSpeed;
+    public bool onGround;
+    public float force;
+
+    public bool gravity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onGround)
+            {
+                print("jump!");
+                rb.AddForce(new Vector3(0f, -JumpSpeed, 0f), ForceMode2D.Impulse);
+                //rb.AddTorque(Random.Range(-50, 50));
+                onGround = false;
+                //AudioSource.PlayOneShot(Jump);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (gravity == true)
+            {
+                rb.gravityScale *= -1;
+                gravity = false;
+            }
+            if (gravity == false)
+            {
+                rb.gravityScale *= 1;
+                gravity = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("colliding");
+          onGround = true;
+        }
+    }
+
+}
